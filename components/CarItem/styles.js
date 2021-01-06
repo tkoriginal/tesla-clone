d051b782bd9408769127913fda13a28e050139d8
Tauqueer <tauqueer.khan@gmail.com>
2021-01-06 15:05:09 -0500

    add car item component

---
+++ components/CarItem/styles.js
@@ -0,0 +1,34 @@
+
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  carContainer: {
+    width: '100%',
+    height: '100%',
+  },
+  titles: {
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: '500',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#5c5e62'
+  },
+  backgroundImage: {
+    height: '100%',
+    width: '100%',
+    resizeMode: 'cover',
+    position: 'absolute'
+  }
+});
